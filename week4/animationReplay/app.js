6b1bfbb4d6a6f0b4e4cfae393ed1f5538fcee73e
Jesse Schollitt <jschollitt@unitec.ac.nz>
2025-07-30 16:46:12 +1200

    Semester 2 2025 begin

--- week4/animationReplay/app.js
+++
@@ -1,5 +0,0 @@
-function adReplay() {
-    let ad = document.getElementById("adWrapper");
-    let copy = ad.cloneNode(true);
-    ad.replaceWith(copy);
-}
--- week8/assets/js/iframeScale.js
+++
@@ -1,4 +0,0 @@
-function resizeIFrame(object) {
-    object.style.height = object.contentWindow.document.documentElement.scrollHeight + 'px';
-    console.log("resize iframe");
-}
