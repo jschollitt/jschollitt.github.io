5fb45b2ebc4e612a9e08ae8515b2431d8eb28824
Jesse Schollitt <dev@jschollitt.net>
2026-02-25 12:44:05 +1300

    Semester 1 2026

--- week4/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-const handleFormSubmission = function (e) {
-    let fname = document.querySelector("#fname").value;
-    let lname = document.querySelector("#lname").value;
-    let dob = document.querySelector("#dob").value;
-    let pword = document.querySelector("#pword").value;
-
-    alert(`Name: ${fname} ${lname}\nDate of birth: ${dob}\nPassword: ${pword}`);
-}
-
